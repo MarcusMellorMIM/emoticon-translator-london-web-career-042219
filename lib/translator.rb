2bb3b6a952c07c2e63dca24cbf09e7a2be7ecd1b
Marcus Mellor <marcus@mellor.com>
2019-03-30 14:06:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,13 +9,12 @@ def load_library( file_path )
     "get_meaning" => {}
   }
   
-  puts file_path
-  
+
   emoticons=YAML.load_file( file_path )
 
-  puts emoticons
-  
+
   emoticons.each do | k, v|
+    puts k
     #assuming key is meaning, 0 is emoticon and 1 is japanese in an array
     uk_emot=emoticons[k][0]
     jap_emot=emoticons[k][1]
