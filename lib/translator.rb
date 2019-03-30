a5add423058e335c3c0ffc464bf4a238c4b6a011
Marcus Mellor <marcus@mellor.com>
2019-03-30 14:11:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ require "yaml"
 def load_library( file_path )
   # code goes here
   require "yaml"
+  
   return_hash={
     "get_emotion" => {},
     "get_meaning" => {}
@@ -13,18 +14,14 @@ def load_library( file_path )
   emoticons=YAML.load_file( file_path )
 
 
-  emoticons.each do | k, v|
-    puts k
-    puts v
+    emoticons.each do | k, v|
+    
     #assuming key is meaning, 0 is emoticon and 1 is japanese in an array
-    uk_emot=emoticons[k][0]
-    puts uk_emot
-    jap_emot=emoticons[k][1]
-    puts jap_emot
-    return_hash["get_emotion"][uk_emot]=jap_emot
-    return_has["get_meaning"][jap_emot]=k
-    puts "added to return hash"  
-  end
+      uk_emot=emoticons[k][0]
+      jap_emot=emoticons[k][1]
+      return_hash["get_emotion"][uk_emot]=jap_emot
+      return_hash["get_meaning"][jap_emot]=k
+    end
   
   return_hash
 
